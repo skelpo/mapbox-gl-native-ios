1ae31cb4d37091fc7b898d8b6a4c2b111c8be0b2
Caleb Kleveter <caleb.kleveter@gmail.com>
2020-08-28 12:38:58 -0500

    Changed library name to Mapbox

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ import Foundation
 let _package = Package(
     name: "mapbox-gl-native-ios",
     products: [
-        .library(name: "mapbox-gl-native-ios", targets: ["Mapbox"]),
+        .library(name: "Mapbox", targets: ["Mapbox"]),
     ],
     dependencies: [
         .package(url: "https://github.com/skelpo/mapbox-events-ios.git", .branch("master")),
